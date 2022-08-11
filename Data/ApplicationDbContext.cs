36836dcf41ab92e9af383648a14694e083fbcba6
Shadman Arko <35429182+shadmanArko@users.noreply.github.com>
2022-08-11 14:48:56 +0600

    Update ApplicationDbContext.cs

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,6 +1,24 @@
-﻿namespace BulkyBookWeb.Data
+﻿using BulkyBookWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyBookWeb.Data
 {
-    public class ApplicationDbContext
+    public class ApplicationDbContext : DbContext
     {
+
+        /// <summary>
+        /// This method will receive "options" and send it to base DbContext
+        /// </summary>
+        /// <param name="options"></param>
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+
+        /// <summary>
+        /// This will create the table
+        /// </summary>
+        public DbSet<Category> Categories { get; set; }
     }
 }
