b0579a51f40557406dc2767d044f01c1fce40945
Shadman Arko <35429182+shadmanArko@users.noreply.github.com>
2022-08-12 12:31:05 +0600

    Edit Category Get
    1. In Category Controller Added Edit Get And Edit Post
    2. Created Edit View
    3. Updated Edit according to create view.
    4. In Index added a new table data for binding and showing date for database and get the data for edit.

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -43,5 +43,41 @@ namespace BulkyBookWeb.Controllers
             }
             return View(obj);
         }
+
+        //Get
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id ==0)
+            {
+                return NotFound();
+            }
+
+            var categoryFromDb = _db.Categories.Find(id);
+
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+
+            return View(categoryFromDb);
+        }
+
+        //post
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("Name", "The DisplayOrder cannot exactly match the Name");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
     }
 }
